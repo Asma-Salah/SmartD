ed94742012158e941d10a166df28b466ed42a61f
Asma-Salah <asmaysalah2030@gmail.com>
2026-02-05 12:03:52 +0300

    In Product.ts, changed Product definition from type to interface.

--- smarts-frontend/src/types/Product.ts
+++ smarts-frontend/src/types/Product.ts
@@ -1,7 +1,7 @@
-export type Product = {
-    id: number;
-    name: string; 
-    price: number;
-    image: string;
-    category: string;
+export interface Product {
+    id: number,
+    name: string, 
+    price: number,
+    image: string,
+    category: string,
 };
