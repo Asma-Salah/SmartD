fae48adb96f200a66c92a83111dd24ef76703d0d
Asma-Salah <asmaysalah2030@gmail.com>
2026-02-06 16:06:49 +0300

    Update dependencies and enhance Products page with new category filter

--- smarts-frontend/src/components/ProductCard.tsx
+++ smarts-frontend/src/components/ProductCard.tsx
@@ -4,29 +4,26 @@ import {
   CardMedia,
   Typography,
   Button,
-  CardActions
+  CardActions,
 } from "@mui/material";
 import type { Product } from "../types/Product";
-import { Link } from "react-router-dom";  
+import { Link } from "react-router-dom";
 type ProductCardProps = {
   product: Product;
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <Card sx={{ maxWidth: 300  }}>
+    <Card sx={{ maxWidth: 300 }}>
       <CardMedia
         component="img"
         height="180"
         image={product.image}
         alt={product.name}
-        
       />
 
       <CardContent>
-        <Typography variant="h6">
-          {product.name}
-        </Typography>
+        <Typography variant="h6">{product.name}</Typography>
 
         <Typography variant="body2" color="text.secondary">
           KES {product.price}
@@ -34,7 +31,12 @@ export default function ProductCard({ product }: ProductCardProps) {
       </CardContent>
 
       <CardActions>
-        <Button size="small" variant="contained" component = {Link} to ={`/products/${product.id}`}>
+        <Button
+          size="small"
+          variant="contained"
+          component={Link}
+          to={`/products/${product.id}`}
+        >
           View
         </Button>
       </CardActions>
--- smarts-frontend/src/pages/Products.tsx
+++ smarts-frontend/src/pages/Products.tsx
@@ -1,68 +1,65 @@
-
-import { Grid , Stack,Button, Box,} from "@mui/material";
+import { Grid, Stack, Button, Box } from "@mui/material";
 import ProductCard from "../components/ProductCard";
-
-
 
 import { useState } from "react";
 import { products } from "../data/products";
 
-
-
-
-
 export default function Products() {
   const [selectedCategory, setSelectedCategory] = useState<
-  "All" | "Phones" | "Accessories"
->("All");
+    "All" | "Phones" | "Accessories" | "wearables"
+  >("All");
 
-const filteredProducts =
-  selectedCategory === "All"
-    ? products
-    : products.filter(
-        (product) => product.category === selectedCategory
-      );
-
-
-
-
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : products.filter((product) => product.category === selectedCategory);
 
   return (
-<Box>
-  <Stack direction="row" spacing={2} mb={3}>
-    <Button
-      variant={selectedCategory === "All" ? "contained" : "outlined"}
-      onClick={() => setSelectedCategory("All")}
-    >
-      All
-    </Button>
+    <Box>
+      <Stack direction="row" spacing={2} mb={3}>
+        <Button
+          variant={selectedCategory === "All" ? "contained" : "outlined"}
+          onClick={() => setSelectedCategory("All")}
+        >
+          All
+        </Button>
 
-    <Button
-      variant={selectedCategory === "Phones" ? "contained" : "outlined"}
-      onClick={() => setSelectedCategory("Phones")}
-    >
-      Phones
-    </Button>
+        <Button
+          variant={selectedCategory === "Phones" ? "contained" : "outlined"}
+          onClick={() => setSelectedCategory("Phones")}
+        >
+          Phones
+        </Button>
 
-    <Button
-      variant={selectedCategory === "Accessories" ? "contained" : "outlined"}
-      onClick={() => setSelectedCategory("Accessories")}
-    >
-      Accessories
-    </Button>
-  </Stack>
+        <Button
+          variant={
+            selectedCategory === "Accessories" ? "contained" : "outlined"
+          }
+          onClick={() => setSelectedCategory("Accessories")}
+        >
+          Accessories
+        </Button>
+        <Button
+          variant={selectedCategory === "wearables" ? "contained" : "outlined"}
+          onClick={() => setSelectedCategory("wearables")}
+        >
+          Wearables
+        </Button>
+      </Stack>
 
-    <Grid container spacing={2}>
-      {filteredProducts.map((product) => (
-        <Grid size={{ xs: 12, sm: 6, md: 4 }}   
-        display="flex"
-          justifyContent="center" alignItems="center"
-          key={product.id}>
-          <ProductCard product={product} />
-        </Grid>
-      ))}
-    </Grid>
+      <Grid container spacing={2}>
+        {filteredProducts.map((product) => (
+          <Grid
+            size={{ xs: 12, sm: 6, md: 4 }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            key={product.id}
+          >
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
-  
 }
